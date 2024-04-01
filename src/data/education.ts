b6e19f47196b6d35200fea009d7ade24c476a0ac
Satyam singh <satyamsingh2003a@gmail.com>
2024-04-01 23:17:05 +0530

    created education section

--- src/data/education.ts
+++ src/data/education.ts
@@ -4,10 +4,10 @@ export const EDUCATION: TShowCaseListDetatils[] = [
   {
     title: "Bachelor Of Engineering in Computer Science",
     organisation: {
-      name: "Rizvi College Of Engineering",
+      name: "Thakur  College Of Engineering",
       href: "https://eng.rizvi.edu.in/",
     },
-    date: "2019-2023",
+    date: "2020-2024",
     location: "Kandivali, Mumbai",
     description:
       "Relevant courses included Data Structures and Algorithms, Web Design, Cloud Computing, Artificial Neural Network and Database Management.",
@@ -15,23 +15,23 @@ export const EDUCATION: TShowCaseListDetatils[] = [
   {
     title: "Higher Secondary School",
     organisation: {
-      name: "Mithibai College",
+      name: "St. Anne's Junior College",
       href: "https://www.mithibai.ac.in/",
     },
-    date: "2016-2018",
-    location: "Vile Parle, Mumbai",
+    date: "2018-2020",
+    location: "Vasai, Mumbai",
     description:
-      "Completed the course in Electronics Bifocal Science with 68%. Started freelancing making 3D assets for games. Got interested in game development and learned unity and c#",
+      "Completed the course in Electronics Bifocal Science with 70%. Started freelancing making 3D assets for games. Got interested in game development and learned unity and c#",
   },
   {
     title: "Secondary School Education",
     organisation: {
-      name: "Holy Family High School",
+      name: "St. Francis High School",
       href: "https://holyfamilyandheri.org/",
     },
     date: "2017-2018",
-    location: "Andheri, Mumbai",
+    location: "Vasai, Mumbai",
     description:
-      "Learned good values,dedication and disciplne. Played district football for 5 years.",
+      "Learned good values,dedication and disciplne. Played district football for 2 years.",
   },
 ];
