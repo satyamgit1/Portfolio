c46ad9e6f153f460e237bd913a6c24f7492a0885
Satyam singh <satyamsingh2003a@gmail.com>
2024-04-16 21:13:19 +0530

    project addd with some css

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -46,80 +46,29 @@ export const PROJECT_SHOWCASE: ProjectShowcaseListProps[] = [
 export const PROJECTS_CARD: ProjectCardProps[] = [
   {
     name: "SocietySathi",
-    favicon: "/images/projects/logos/jsontree.ico",
+    favicon: "/images/projects/logos/societylogo.ico",
     imageUrl: [
       "/images/projects/jsontreeLight.webp",
-      "/images/projects/jsontreeDark.webp",
+      "/images/projects/societysathi2.webp",
     ],
     description:
-      "Visualize JSON data format in form of a tree or graph. Application is made using Nextjs, Tailwindcss and Monaco Editor. Design image can be downloaded from this app.",
-    sourceCodeHref: "https://github.com/BUMBAIYA/jsontree",
-    liveWebsiteHref: "https://jsontree.vercel.app",
+      "Introducing SocietySathi - Your Ultimate Apartment Management Solution! 🏢  Excited to share SocietySathi, a robust Apartment Management System built with Next.js, Express, Node.js, and MongoDB. 🚀",
+    sourceCodeHref: "https://github.com/rhtm123/ff-frontend",
+    liveWebsiteHref: "https://www.societysathi.com/",
   },
   {
     name: "Bhagavad-gita",
-    favicon: "/images/projects/logos/kanban.ico",
+    favicon: "/images/projects/logos/gitalogo.ico",
     imageUrl: [
       "/images/projects/kanbanLight.webp",
-      "/images/projects/kanbanDark.webp",
-      "/images/projects/kanbanCardLight.webp",
+      "/images/projects/gita2.webp",
+      "/images/projects/bhagavadgita2.webp",
+      "/images/projects/gitacontact.webp",
+      "/images/projects/gitaaudio.webp",
     ],
     description:
-      "Keep track of projects and tasks in different categories. Drag and drop card from tables. Assign user and priority to task and many more features. This app is made using Reactjs, Typescript, Tailwindcss and React-beautiful-dnd library.",
-    sourceCodeHref: "https://github.com/BUMBAIYA/kanban",
-    liveWebsiteHref: "https://kannban-board.vercel.app",
-  },
-  {
-    name: "Many Games",
-    favicon: "/images/projects/logos/manygames.ico",
-    imageUrl: [
-      "/images/projects/manyGamesDark.webp",
-      "/images/projects/manyGamesLight.webp",
-      "/images/projects/manyGames2048.webp",
-      "/images/projects/manyGamesPuzzle.webp",
-      "/images/projects/manyGamesWordle.webp",
-    ],
-    description:
-      "Many short and fun games using Reactjs, Typescript and Tailwindcss with collaboration with other developers",
-    sourceCodeHref: "https://github.com/BUMBAIYA/ManyGames",
-    liveWebsiteHref: "https://manygames.vercel.app",
-  },
-  {
-    name: "My portfolio",
-    favicon: "/favicon.ico",
-    imageUrl: [
-      "/images/projects/portfolioDark.webp",
-      "/images/projects/portfolioLight.webp",
-    ],
-    description:
-      "My personal portfolio website made using Nextjs, tailwindcss and framer motion.",
-    sourceCodeHref: "https://github.com/BUMBAIYA/amitchauhan-v2",
-    liveWebsiteHref: siteMetadata.siteUrl,
-  },
-  {
-    name: "Covid Tracker",
-    favicon: "/images/projects/logos/covidtracker.ico",
-    imageUrl: [
-      "/images/projects/covidTracker.webp",
-      "/images/projects/covidTrackerMap.webp",
-      "/images/projects/covidTrackerTable.webp",
-    ],
-    description:
-      "Get latest covid related stats in a table where user can filter using country and cases. It also has a map which show every countries data for quick view. It uses an open source api (disease.sh). Made using Reactjs, Javascript and MUI",
-    sourceCodeHref: "https://github.com/BUMBAIYA/CovidTracker",
-    liveWebsiteHref: "https://bumbaiya.github.io/CovidTracker",
-  },
-  {
-    name: "Stock predictor",
-    favicon: "/images/projects/logos/stockpredictor.ico",
-    imageUrl: [
-      "/images/projects/stockPredictor.webp",
-      "/images/projects/stockPredictorCandleChart.webp",
-      "/images/projects/stockPredictorCompareChart.webp",
-      "/images/projects/stockPredictorLineChart.webp",
-    ],
-    description:
-      "Get prediction of opening and closing price of stocks price. Frontend is made using Reactjs and backend is made using Flask and Machine learning model are trained using Tensorflow",
-    sourceCodeHref: "https://github.com/Rohrschachh/Stock-Market-Predictor",
+      "We developed a website centered around the Hindu scripture Bhagavad Gita using Next.js and Tailwind CSS. Integrated API calls to retrieve and display chapter and verse data dynamically. The project showcases proficiency in front-end technologies and a commitment to cultural and religious exploration through digital platforms.",
+    sourceCodeHref: "https://github.com/satyamgit1/bhagavad_gita",
+    liveWebsiteHref: "https://www.bhagavadgita.site/",
   },
 ];
