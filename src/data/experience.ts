01d3a53685883086f53a840e287c23e59539cacf
Satyam singh <satyamsingh2003a@gmail.com>
2024-06-17 01:54:29 +0530

    added new experiences

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,32 +5,34 @@ export const EXPERIENCE: TShowCaseListDetatils[] = [
     title: "Full Stack Developer(MERN)",
     organisation: {
       name: "Coding Chaska",
-      href: "https://skima.ai/",
+      href: "https://www.codingchaska.com/",
     },
-    date: "Oct 2023 - Present",
+    date: "Sept 2023 – Jan 2024",
     location: "Mumbai",
-    description: "Reactjs and Nextjs app development",
+    description: "Created in-depth coding solutions and blog posts covering diverse topics, resulting in a 30% increase in user retention and a15% growth in monthly active users\n" +
+    "● Built and integrated a quiz page with real-time performance tracking metrics to automate evaluation processes, leading toincreased student retention rates and improved learning outcomes.\n" +
+    "● Directed the development of a high-impact scholarship program webpage, resulting in a 40% increase in student applicationsand rise in enrollment rates, effectively boosting the institute’s student outreach and growth initiatives.",
   },
   {
-    title: "Engineering Trainee",
+    title: "SDE - Frontend Developer",
     organisation: {
-      name: "Hexaware",
-      href: "https://hexaware.com/",
+      name: "Growth Farm",
+      href: "", // Add closing double quote here
     },
     date: "July 2023 - Sep 2023",
     location: "Remote",
     description:
-      "Learned to work we monorepo and got to learn application architecture, scalling and build tooling to improve CI/CD of micro services.",
+      "Worked on Next.js framework to build SEO ready educational website from scratch for grade 6 to 12 with questions, video & numerical for students.● Learned & worked on new technologies such as supertoken auth.● Utilized Amazon S3 for storing course thumbnails, program materials, and feature images, ensuring efficient and scalable management of media assets.● Deployed Project in AWS and backend server for better Availability and Scalability.",
   },
   {
-    title: "Frontend Developer Intern",
+    title: "Frontend Developer",
     organisation: {
-      name: "Master Infotech",
-      href: "https://masterinfotech.com/",
+      name: "Web Professionals",
+      href: "https://www.webp.com.au/",
     },
-    date: "Winter - 2022",
-    location: "Remote",
+    date: "Feb 2024 – Mar 2024",
+    location: "offline",
     description:
-      "Worked on a team responsible for developing new features and updating old codebases to latest technologies. Learned alot about web development and CI/CD development cycle.",
+      "Built JNI Interiors website with Next.js and Node.js. integrated “GET Quote” feature for enhanced user engagement and seamless client communication. Improved SEO to increase search engine rankings and drive more organic traffic",
   },
 ];
